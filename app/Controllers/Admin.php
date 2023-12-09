66650d9165ea316fad273237241735139b087042
Farma <54702653+Farma27@users.noreply.github.com>
2023-12-09 13:53:54 +0700

    Login Page

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -1,39 +1,115 @@
 <?php
 namespace App\Controllers;
 
-class Admin extends BaseController{
+use Myth\Auth\Models\UserModel;
+
+class Admin extends BaseController
+{
     protected $db, $builder;
-    public function __construct(){
-        $this->db      = \Config\Database::connect();
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
         $this->builder = $this->db->table('users');
     }
-    public function index(){
+    public function index()
+    {
         $this->builder->select('users.id as userid, username, email, name');
         $this->builder->join('auth_groups_users', 'auth_groups_users.user_id = users.id');
         $this->builder->join('auth_groups', 'auth_groups.id = auth_groups_users.group_id');
-        
+
         $query = $this->builder->get();
-        $data =[
+        $data = [
             'title' => 'SGC - Admin Page',
             'users' => $query->getResult()
         ];
         return view('admins/index', $data);
     }
-    public function detail($id = 0){
+    public function detail($id = 0)
+    {
         $this->builder->select('users.id as userid, username, email, name, fullname, user_image');
         $this->builder->join('auth_groups_users', 'auth_groups_users.user_id = users.id');
         $this->builder->join('auth_groups', 'auth_groups.id = auth_groups_users.group_id');
         $this->builder->where('users.id', $id);
         $query = $this->builder->get();
-        $data =[
+        $data = [
             'title' => 'SGC - Admin Page',
             'user' => $query->getRow()
         ];
 
-        if(empty($data['user'])){
+        if (empty($data['user'])) {
             return redirect()->to('/admin');
         }
 
         return view('admins/detail', $data);
     }
+
+    public function tambahUser()
+    {
+        $data = [
+            'title' => 'Mendaftarkan User User'
+        ];
+        return view('admins/addUsers', $data);
+    }
+
+    public function simpanUser()
+    {
+        $usermyth = new UserModel();
+        $usermyth->withGroup($this->request->getVar('role'))->save([
+            'fullname' => $this->request->getVar('fullname'),
+            'username' => $this->request->getVar('username'),
+            'email' => $this->request->getVar('email'),
+            'password' => Password::hash('password'),
+            'active' => 1
+        ]);
+        $data = [
+            'title' => 'Mendaftarkan User User'
+        ];
+        return view('admins/addUsers', $data);
+    }
+
+    public function ubahAkses($id)
+    {
+        // Load the necessary models or services
+        $userModel = new \Myth\Auth\Models\UserModel();
+
+        // Retrieve the list of users and groups
+        $data['users'] = $userModel->find($id);; // Ini adalah contoh, sesuaikan dengan kebutuhan Anda
+
+        // Load the view and pass the data
+        return view('admins/ubahAkses', $data);
+    }
+
+    public function changeUserGroup($id, $newGroupId)
+    {
+        // Load the authentication library
+        $auth = service('authentication');
+
+        // Check if the user is logged in as an admin or has the necessary permission
+        if ($auth->isLoggedIn() && $auth->inGroup('admin')) {
+            // Load the user model
+            $userModel = new \Myth\Auth\Models\UserModel();
+
+            // Retrieve the user by user ID
+            $user = $userModel->find($id);
+
+            // Check if the user exists
+            if ($user) {
+                // Change the user's group
+                $auth->addUserToGroup($user->id, $newGroupId);
+
+                // Optionally, you can remove the user from the old group
+                // $auth->removeUserFromGroup($user->id, $oldGroupId);
+
+                // Redirect or return a success message
+                return redirect()->to('dashboard')->with('success', 'User group updated successfully');
+            } else {
+                // User not found, redirect or return an error message
+                return redirect()->back()->with('error', 'User not found');
+            }
+        } else {
+            // Redirect or return an error message for unauthorized access
+            return redirect()->back()->with('error', 'Unauthorized access');
+        }
+    }
+
 }
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -2,87 +2,97 @@
 
 <?= $this->section('content') ;?>
 
-    <div class="container">
+<div class="container position-absolute top-50 start-50 translate-middle">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-            <div class="col-md-6">
+        <div class="col-md-6">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+            <div class="card overflow-hidden border-0 shadow-lg">
+                <div class="card-body p-0">
 
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4"><?=lang('Auth.loginTitle')?></h1>
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg">
+                            <div class="p-4">
+                                <div class="text-center">
+                                    <h1 class="title text-gray-900 fw-bold mb-3">SGCommunity</h1>
+                                    <h1 class="subtitle text-gray-900 fw-light">Welcome</h1>
+                                    <img src="<?= base_url('../img/logoSGC.png') ?>" alt="Logo SGC" class="logo">
+                                </div>
+                                <?= view('Myth\Auth\Views\_message_block') ?>
+                                <form class="user" action="<?= url_to('login') ?>" method="post">
+                                <?= csrf_field() ?>
+
+                                <?php if ($config->validFields === ['email']): ?>
+                                    <!-- Username/Email -->
+                                    <div class="input-group border-0 mb-3">
+                                        <span class="input-group-text border-0" id="user"><i class="fa-solid fa-user"></i></span>
+                                        <input type="text" class="form-control form-control-user border-0 <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+                                            aria-describedby="emailHelp"
+                                            name="login" placeholder="<?=lang('Auth.email')?>">
+                                            <div class="invalid-feedback">
+                                                <?= session('errors.login') ?>
+                                            </div>
                                     </div>
-                                    <?= view('Myth\Auth\Views\_message_block') ?>
-                                    <form class="user" action="<?= url_to('login') ?>" method="post"
-                                    >
-                                    <?= csrf_field() ?>
-
-                                    <?php if ($config->validFields === ['email']): ?>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
-                                                aria-describedby="emailHelp"
-                                                name="login" placeholder="<?=lang('Auth.email')?>">
+                                    <?php else: ?>
+                                        <div class="input-group border-0 mb-3">
+                                            <span class="input-group-text border-0" id="user"><i class="fa-solid fa-user"></i></span>
+                                            <input type="text" class="form-control form-control-user border-0 <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+                                                name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
                                                 <div class="invalid-feedback">
                                                     <?= session('errors.login') ?>
                                                 </div>
                                         </div>
-                                        <?php else: ?>
-                                            <div class="form-group">
-                                                <input type="text" class="form-control form-control-user <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
-                                                    name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
-                                                    <div class="invalid-feedback">
-                                                        <?= session('errors.login') ?>
-                                                    </div>
-                                            </div>
-                                        <?php endif; ?>
-                                        <div class="form-group">
-                                            <input type="password" name="password" class="form-control form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?=lang('Auth.password')?>">
-                                            <div class="invalid-feedback">
-                                                <?= session('errors.password') ?>
-                                            </div>
+                                    <?php endif; ?>
+
+                                    <!-- Password -->
+                                    <div class="input-group border-0 mb-3">
+                                        <span class="input-group-text border-0" id="user"><i class="fa-solid fa-lock"></i></span>
+                                        <input type="password" name="password" class="form-control border-0 form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?=lang('Auth.password')?>">
+                                        <div class="invalid-feedback">
+                                            <?= session('errors.password') ?>
                                         </div>
-                                        <?php if ($config->allowRemembering): ?>
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked <?php endif ?>>
-                                                    <?=lang('Auth.rememberMe')?>
-                                                </label>
-                                            </div>
-                                        <?php endif; ?>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block"><?=lang('Auth.loginAction')?></button>
-                                        
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                    <?php if ($config->allowRegistration) : ?>
-                                        <p><a href="<?= url_to('register') ?>"><?=lang('Auth.needAnAccount')?></a></p>
-                                    <?php endif; ?>
                                     </div>
-                                    <div class="text-center">
-                                    <?php if ($config->activeResetter): ?>
-                                        <p><a href="<?= url_to('forgot') ?>"><?=lang('Auth.forgotYourPassword')?></a></p>
+                                    <?php if ($config->allowRemembering): ?>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked <?php endif ?>>
+                                                <?=lang('Auth.rememberMe')?>
+                                            </label>
+                                        </div>
                                     <?php endif; ?>
+
+                                    <div class="btn-login d-flex justify-content-center mx-5">
+                                        <button type="submit" class="btn btn-primary btn-user btn-block" id="btn-login"><?=lang('Auth.loginAction')?></button>
                                     </div>
-                                    <p>
-                                            Made by Kelompok 3
-                                        </p>
+                                    
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                <?php if ($config->allowRegistration) : ?>
+                                    <p><a href="<?= url_to('register') ?>"><?=lang('Auth.needAnAccount')?></a></p>
+                                <?php endif; ?>
                                 </div>
+                                <div class="text-center">
+                                <?php if ($config->activeResetter): ?>
+                                    <p><a href="<?= url_to('forgot') ?>"><?=lang('Auth.forgotYourPassword')?></a></p>
+                                <?php endif; ?>
+                                </div>
+                                <p class="text-center">
+                                    Made by Kelompok 3
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
 
     </div>
+
+</div>
 
 <?= $this->endSection() ;?>
